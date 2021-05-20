6a7202b557a0b12715dc172cc7d88d85aea699b5
Robert Vári <mail.robertvari@gmail.com>
2021-05-20 12:11:47 +0200

    init

---
+++ 01_define_a_function.py
@@ -0,0 +1,13 @@
+def print_hello():
+    print("Hello")
+
+def print_my_name():
+    print("Robert")
+
+def print_my_address():
+    print("Budapest")
+
+
+print_my_address()
+print_my_name()
+print_hello()
