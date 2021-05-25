af3dffa40a1d22ff22ab7244304e7e0b2e00b3de
Robert Vári <mail.robertvari@gmail.com>
2021-05-25 13:04:37 +0200

    phonebook init

---
+++ phonebook/main.py
@@ -0,0 +1,9 @@
+from utilities import phonebook_utils
+
+
+def main():
+    print("-"*50, "Phonebook", "-"*50,)
+
+
+if __name__ == '__main__':
+    main()
---
+++ phonebook/utilities/phonebook_utils.py
@@ -0,0 +1,18 @@
+def menu():
+    print("Menu")
+
+
+def load_phonebook():
+    print("load_phonebook")
+
+
+def edit_phonebook():
+    print("edit_phonebook")
+
+
+def save_phonebook():
+    print("save_phonebook")
+
+
+def delete_phonebook():
+    print("save_phonebook")
