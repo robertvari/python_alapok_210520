e6ad834441a8f78ac0f11799acd1a57160db26bc
Robert Vári <mail.robertvari@gmail.com>
2021-05-25 13:16:56 +0200

    phonebook init

--- phonebook/main.py
+++ phonebook/main.py
@@ -1,8 +1,10 @@
-from utilities import phonebook_utils
+from utilities import phonebook_utils as utils
 
 
 def main():
     print("-"*50, "Phonebook", "-"*50,)
+
+    user_choice = utils.menu()
 
 
 if __name__ == '__main__':
--- phonebook/utilities/phonebook_utils.py
+++ phonebook/utilities/phonebook_utils.py
@@ -1,9 +1,22 @@
-def menu():
-    print("Menu")
+def menu() -> str:
+    print(
+        "1. Print Phonebook\n"
+        "2. Find Contact\n"
+        "3. Add Contact\n"
+        "4. Edit Contact\n"
+        "5. Delete Contact\n"
+        "6. Exit"
+    )
+
+    return input()
 
 
 def load_phonebook():
     print("load_phonebook")
+
+
+def add_contact():
+    print("Add contact")
 
 
 def edit_phonebook():
@@ -16,3 +29,7 @@ def save_phonebook():
 
 def delete_phonebook():
     print("save_phonebook")
+
+
+if __name__ == '__main__':
+    menu()
